9332c92e9627df5ff3613ff9d07b40f439e9024b
himanshu <himanshukr98040@gmail.com>
2021-01-26 18:44:20 +0530

    26-01-2021

---
+++ arthimatic.c
@@ -0,0 +1,20 @@
+/* Calculstion of diffrent arithmetic */ 
+ 
+ #include<stdio.h>
+ 
+ int main()
+ {
+ 	int a, 
+ 	float b,
+ 	
+ 	a = 46;
+ 	b = 4.5;
+ 	
+ 	
+ 	printf("a + b = %d\n" , a+b);
+ 	printf("a * b = %d\n" , a*b);
+ 	printf("a - b = %d\n" , a-b);
+ 	printf("a / b = %d\n" , a/b);
+ 	
+ 	return 0;
+ }
