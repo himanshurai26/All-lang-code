134b1964a5fc0b8862310e021a51128c37b37a3e
himanshu <himanshukr98040@gmail.com>
2021-01-26 12:02:11 +0530

    26-01-2021

---
+++ substraction.c
@@ -0,0 +1,16 @@
+/* calculation of substraction */
+
+#include<stdio.h>
+int main ()
+{
+	int a, b ;
+	a = 15 ; 
+	b = 6 ;
+	
+	printf("%d\n", &a);
+	printf("d\n" , &b);
+	
+	printf("the substract %d\n", a-b);
+	
+	return 0;
+}
