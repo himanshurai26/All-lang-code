2704574c0cbf6963e8d427856963f1e73abcb758
himanshu <himanshukr98040@gmail.com>
2021-01-27 22:09:44 +0530

    learning coding

---
+++ lernig_coding.c
@@ -0,0 +1,14 @@
+/* testing of int and float */
+#include<stdio.h>
+
+int main()
+{
+	int a = 6;
+	float b = 5.567;
+	
+	printf("himanshu\n");
+	printf("the value of a is %d\n and the value of b is %f\n", a, b);
+	
+	return 0;
+	
+}
---
+++ tutorial_sum.c
@@ -0,0 +1,29 @@
+/*
+ print multiplication table of a number entered by the user in pretty form 
+
+examole:
+
+input
+enter the number you want multiplication table of:
+6
+output:
+table of 6:
+6 x 1 = 6
+6 x 2 = 12
+6 x 3 = 18
+6 x 4 = 24
+6 x 5 = 30
+6 x 6 = 36
+6 x 7 = 42
+6 x 8 = 48
+6 x 9 = 54
+6 x 10 = 60
+*/ 
+
+include<stdio.h>
+
+int main()
+{
+	printf("enter the number you want multiplication table of\n")
+	return 0;
+}
