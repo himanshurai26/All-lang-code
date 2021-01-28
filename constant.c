b7f2dc7bb8793e73fef1061e2fd9933d4f09241f
himanshu <himanshukr98040@gmail.com>
2021-01-28 20:48:31 +0530

    28-01-2021

---
+++ constant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+	int a = 6;
+	const float b = 7.333;
+/*	PI = 7.333; can not do this since PI is constant */
+ /* author himanshu kumar date - 28-01-2021 */
+	/* backslash \a is used for pc sound */
+	
+	printf(" tab character \t\ my backslash \a \\n%f", PI);
+	
+	return 0;
+}
---
+++ testing4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 6;
+	float b = 5.438;
+	
+	printf("%18.4f this" , b);
+	
+	return 0;
+}
