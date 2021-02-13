137ba6cb72af736923e45d7becae46dbb60166be
himanshu <himanshukr98040@gmail.com>
2021-02-13 21:59:07 +0530

    13-0202021

---
+++ age.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+	int age; 
+	printf("enter your age\n");
+	scanf("%d", &age);
+	
+	switch (age)
+	{
+		case 3:
+			printf("the age is 3\n");
+			
+		case 13:
+		    printf("the age is 13\n");
+			
+		case23:
+			printf("the age is 23\n");
+			break;
+		
+		default:
+		printf("the age is not 3, 13 or 23");		    		
+			
+	    		
+			
+	}
+	
+	
+    return 0;
+}
