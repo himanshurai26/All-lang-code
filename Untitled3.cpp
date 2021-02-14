b782a44bba48b4de2c43b639942f17e7d9344330
himanshu <himanshukr98040@gmail.com>
2021-02-14 14:59:45 +0530

    14-02-2021

---
+++ Untitled3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("himanshurai");
+	int i;
+	for(i=0,i<5; i++)
+	{
+		printf("%d", i);
+	}
+	
+	return 0;
+}
---
+++ for_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("himanshurai");
+	int j,i;
+	for(i=0, j=0;i<5;i++)
+	{
+		printf("%d %d\n", i,j);
+	}
+	
+	return 0;
+}
