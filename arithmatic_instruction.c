c1f91df7b78702a0d3ffe9de4b952cd21c0d1526
himanshu <himanshukr98040@gmail.com>
2021-02-22 21:20:39 +0530

    22-02-2021

---
+++ arithmatic_instruction.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int a = 5;
+	int b = 3;
+	printf("the value of a + b is %d\n", a + b);
+	printf("the value of a - b is %d\n", a - b);
+	printf("the value of a * b is %d\n", a * b);
+	printf("the value of a / b is %d\n", a / b);
+	printf("5when divide by 2 leaves the remainder of %d\n", 5%2);
+	printf("-5 divide by 2 leaves the remainder of %d\n", -5%2);
+    printf("-5when divide by -2 leaves the remainder of %d\n", -5%-2);
+
+			
+	return 0;
+	
+}
---
+++ chapter 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	float a = 2.2;
+	float b = a + 7.8;
+	
+    printf("the value of b is %f\n", b);
+	
+	
+	return 0;
+}
